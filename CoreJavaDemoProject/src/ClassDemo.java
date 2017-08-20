484d08aff124a1ce882a0f65a2cca3c22ba52606
kumarvimal0503 <kumar.vimal0503@gmail.com>
2017-08-20 14:15:47 +0530

    Example

--- CoreJavaDemoProject/src/ClassDemo.java
+++ CoreJavaDemoProject/src/ClassDemo.java
@@ -1,3 +1,4 @@
+import inheritance.A;
 
 public class ClassDemo {
 
---
+++ CoreJavaDemoProject/src/inheritance/MethodOverridingDemo.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+public class MethodOverridingDemo {
+	
+	public static void main(String[]args){
+		A a = new B();
+		a.display();
+	}
+	
+}
+
+
+
+class A {
+	
+	public void display(){
+		System.out.println("In A"); 
+	}
+	
+} 
+
+class B extends A{
+	
+	public void display(){
+		System.out.println("In B");
+	}
+	
+}
